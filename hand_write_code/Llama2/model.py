5e85e3ff5541f0bda965c64850de68d8f724145d
chenyixuan <glpgsllx@163.com>
2024-08-20 23:21:34 +0800

    旋转位置编码，图片增强

--- hand_write_code/Llama2/model.py
+++ hand_write_code/Llama2/model.py
@@ -5,67 +5,59 @@ import math
 from dataclasses import dataclass
 from typing import Optional
 
-
 @dataclass
 class ModelArgs:
     dim: int = 4096
-    n_layers: int = 32
-    n_heads: int = 32  # number of heads for Q
-    n_kv_heads: Optional[int] = None  # number of heads for K and v
-    vocab_size: int = -1  # set when we load the tokenizer
-    multiple_of : int = 256
-    ffn_dim_multiplier: Optional[float] = None
+    n_layer: int = 32
+    n_heads: int = 32 # heads for Queries
+    n_kv_heads: Optional[int] = None # heads for Keys and Values'
+    vocab_size: int = -1 # defined later
+    multiple_of: int = 256
+    fnn_dim_multiplier: Optional[float] = None
     norm_eps: float = 1e-5
 
-    # Needed for KV-Cache
+    # Needed for KV Cache
     max_batch_size: int = 32
     max_seq_len: int = 2048
 
-    device: str = None
+    device: str=None
 
 
 def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, theta: float = 10000.0):
-    assert head_dim % 2 == 0, "Rotational Positional Encoding cannot be applied to odd dimensions"
-    # Build the theta parameters
-    # According to the formula, theta_i = 10000 ^ (-2*(i-1)/dim) for i in [1,2,...,dim/2])
-    # shape: (head_dim / 2, 1)
-    theta_numerator = torch.arange(0, head_dim, 2).float() # [0, 2, 4, ... dim-2]
-    # shape: (head_dim / 2, 1)
-    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)
-    # Construct the positions (the "m" parameter)
-    # shape: (seq_len, 1)
-    m = torch.arange(seq_len, device=device)
-    # Multiply each theata by each position using the outer product
-    # shape: (seq_len, 1) outer_product* (head_dim/2, 1) -> (seq_len, head_dim/2)
+    # 旋转位置编码
+    assert head_dim % 2 == 0, "dimension must be even"
+    # theta 参数列表
+    # len: (head_dim / 2), i \in [0, head_dim / 2 - 1]
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+    # theta_i = 10000 ^ (-2(i-1)/dim) for i = [1,2,...,dim/2]
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)  # [theta_1, ....theta_{dim/2}]
+    # Construct the positions (the m parameter)
+    m = torch.arange(seq_len, device=device) # shape: [seq_len]
+    # 每一个位置 m 和 theta_i 的组合
+    # shape: (seq_len, dim/2)
     freqs = torch.outer(m, theta).float()
-    # We can compute complex numbers in the polar form c = R * exp(i * m * theta), where R = 1 as follows:
-    # (seq_len, head_dim/2) -> (seq_len, head_dim/2)
+    # 转化为复数形式计算 c = R * exp(i * m * theta)， where R = 1
+    # (seq_len, dim/2)
     freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_complex
 
-
-def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device: str):
-    # (B, seq_len, H, head_dim) -> (B, seq_len, H, head_dim / 2)
+def apply_rotary_embeddings(x: torch.Tensor, freq_complex: torch.Tensor, device: str):
+    ## as_complex：把最后一维看成复数形式，2变1
+    ## as_real: 把最后一维看成实数，1变2
+    # first step: (B, seq_len, H, dim) -> (B, seq_len, H, dim / 2, 2) -> (B, seq_len, H, dim / 2)
     x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
-    # (seq_len. head_dim/2) -> (1, seq_len, 1, head_dim/2)
-    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
-    # (B, seq_len, H, head_dim/2) * (1, seq_len, 1, head_dim/2) ->(B, seq_len, H, head_dim/2)
+    # (seq_len, dim / 2) -> (1, seq_len, 1, dim / 2)
+    freqs_complex = freq_complex.unsqueeze(0).unsqueeze(2)
+    # (B, seq_len, H, dim / 2) * (1, seq_len, 1, dim/2) -> (B, seq_len, H, dim / 2)
     x_rotated = x_complex * freqs_complex
-    # (B, seq_len, H, head_dim/2) -> (B, seq_len, H, head_dim/2, 2)
+    # (B, seq_len, H, dim / 2) -> (B, seq_len, H, dim/2, 2)
     x_out = torch.view_as_real(x_rotated)
-    # (B, seq_len, H, head_dim/2, 2) -> (B, seq_len, H, head_dim)
+    # (B, seq_len, H, dim/2, 2) -> (B, seq_len, H, dim)
     x_out = x_out.reshape(*x.shape)
     return x_out.type_as(x).to(device)
 
 
-
-
-
-
-
-
 class Transformer(nn.Module):
-    # Architecture of the entire model
     def __init__(self, args: ModelArgs) -> None:
         super().__init__()
 
@@ -77,24 +69,25 @@ class Transformer(nn.Module):
         self.tok_embeddings = nn.Embedding(self.vocab_size, args.dim)
 
         self.layers = nn.ModuleList()
-        for _ in range(args.n_layers):
+        for _ in range(self.n_layers):
             self.layers.append(EncoderBlock(args))
 
         self.norm = RMSNorm(args.dim, eps=args.norm_eps)
         self.output = nn.Linear(args.dim, self.vocab_size, bias=False)
-        # 提前计算位置编码
+
         self.freqs_complex = precompute_theta_pos_frequencies(self.args.dim // self.args.n_heads, self.args.max_seq_len * 2, device=self.args.device)
 
     def forward(self, tokens: torch.Tensor, start_pos: int):
-        # (B, seq_len)
+        # 因为有KV-Cache，只需要给最新的token
+        # （B, seq_len)
         batch_size, seq_len = tokens.shape
-        assert seq_len ==1, "Only one token at a time can be processed"
+        assert seq_len == 1
 
-        # (B, seq_len) -> (B, seq_len, dim)
-        h = self.tok_embeddings(tokens)  # Llama的是4096
+        # (B, seq_len) -> (B, seq_len, Dim)
+        h = self.tok_embeddings(tokens) # 向量化
 
-        # Retrieve the pairs (m, theta) corresponding to the positions [start_pos, start_pos + seq_len]
-        freqs_complex = self.freqs_complex[start_pos:start_pos + seq_len]
+        # retrieve the pairs (m, theta) corresponding to the positions [start_pos, start_pos + seq_len]
+        freqs_complex = self.freqs_complex[start_pos : start_pos + seq_len]
 
         # Consecutively apply all the encoder layers
         for layer in self.layers:
@@ -102,6 +95,7 @@ class Transformer(nn.Module):
         h = self.norm(h)
         output = self.output(h).float()
         return output
+
 
 
 
